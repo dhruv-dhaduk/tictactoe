cc1f1dd4f70107aaaf648b82f9d0edb00555a5be
dhruv-dhaduk <dhadukd44@gmail.com>
2024-03-04 14:42:57 +0530

    Used the state to store the board data

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,18 @@
 import BoardItem from "./BoardItem";
+import { useState } from "react";
 
 function Board() {
+
+    const [board, setBoard] = useState(Array(9).fill(1).map((i) => ( Math.random() < 0.5 ? 0 : 1 )));
 
     return (
         <div className="w-full h-full p-5">
 
             <div className="max-w-full max-h-full m-auto aspect-square grid grid-cols-3 overflow-hidden bg-white bg-opacity-10 backdrop-blur-md border-2 border-white border-opacity-25 rounded-2xl"> 
                 
-                <BoardItem itemState={0} />
-                <BoardItem itemState={1} />
-                <BoardItem itemState={0} />
-                <BoardItem itemState={1} />
-                <BoardItem itemState={0} />
-                <BoardItem itemState={0} />
-                <BoardItem itemState={0} />
-                <BoardItem itemState={1} />
-                <BoardItem itemState={1} />
+                { board.map((value, i) => {
+                    return <BoardItem key={i} itemState={value} />
+                }) }
 
             </div>
 
