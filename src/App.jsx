95f2d538cc4bc1d86706c0b6c3dfa4d045719180
dhruv-dhaduk <dhadukd44@gmail.com>
2024-03-04 14:06:28 +0530

    Created the layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Board from "./components/Board";
 function App() {
 
     return (
-        <div className="bg-black w-full h-dvh">
-            <Board />
+        <div className="bg-black w-full h-fit sm:h-dvh sm:flex sm:items-center sm:justify-center">
+            <div className="sm:flex-1">
+                <Board />
+            </div>
+            <p className="text-red-700 sm:flex-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi numquam neque accusamus. Vitae possimus modi enim culpa animi placeat dolorum inventore distinctio? Perspiciatis sapiente voluptatem repellat beatae odio eos porro consequatur numquam, laboriosam labore, quasi fugit! Possimus enim, voluptate praesentium accusantium consectetur quis soluta cupiditate necessitatibus error, numquam reiciendis vitae aspernatur, architecto dolorem quod! Hic obcaecati a excepturi vero eius deserunt recusandae rem, nisi reiciendis, non earum consectetur dolorum? Consectetur aut nostrum minima, laudantium expedita ipsum ullam rem fugiat magnam reprehenderit, quae necessitatibus distinctio error quasi aliquam vero saepe, quisquam iusto. Quidem consequatur mollitia eos sed error culpa, est voluptatem!</p>
         </div>
 
     );
